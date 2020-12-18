d31ebf76ac2230189c23c3109d13a641c695bb00
Matt Baber <mbaber1142@gmail.com>
2020-12-17 21:23:47 -0500

    added model and api routes, also added html routes and api router to server

---
+++ routes/api-routes.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const router = express.Router();
+const db = require("../models");
+
+router.get("/api/workouts", function (req, res) {
+    db.Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(i => {
+                total += i.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", function (req, res) {
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+});
+
+router.post("/api/workouts", function ({ body }, res) {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", function (req, res) {
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+
+});
+
+
+
+module.exports = router;
