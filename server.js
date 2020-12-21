2dea3c6b19f817e4d93e778623f2a9c9d85fa9c2
Matt Baber <mbaber1142@gmail.com>
2020-12-20 22:18:58 -0500

    added connection

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitnessTracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+);
 
 // require routes
 const routes = require("./routes/api-routes.js");
